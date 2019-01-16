29b9e7a0d17e1f4147c93a40c35c00b822057ff8
novakvova <novakvova@gmail.com>
2019-01-16 16:18:27 +0200

    Hello

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 class App extends React.Component {
 
     state = {
-        carency: []
+        currency: [],
+        isLoad: false,
+        txtValue: {}
     };
 
     //fetch or axios
     componentDidMount() {
+        this.setState({isLoad: true});
         setTimeout(()=>{
-            
+
             fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
             .then(x => {
                 return x.json();
@@ -17,7 +20,8 @@ class App extends React.Component {
             .then(data => {
 
                 console.log("----data----", data);
-                throw new Error("Hello Peter");
+                this.setState({isLoad: false, currency: data});
+                //throw new Error("Hello Peter");
             })
             .catch(er=> {
                 alert(`Помилка ${er.message}`)
@@ -26,13 +30,45 @@ class App extends React.Component {
         
     }
 
+    onHandleChange = (e) => {
+        //console.log("value", e.target.value);
+        this.setState({txtValue: e.target.value});
+    }
+
+    onHandlerSelChange = (e) => {
+        //console.log("---select on change---", e.target.value);
+        const valSel=e.target.value;
+        const ccy=this.state.currency.find((c)=>c.ccy===valSel);
+        if(ccy)
+        {
+            alert(this.state.txtValue*ccy.buy);
+        }
+
+    }
+
     render() {
-        console.log("----props-----", this.state);
+        console.log("----state-----", this.state);
         let p=13;
+        const {isLoad} = this.state;
+
+        const options = this.state.currency.map((c) => {
+            return (
+                <option key={c.ccy}>{c.ccy}</option>
+            );
+        });
         return (
             <div>
                 <h1>Hello {p}</h1>
                 <h2>Peter</h2>
+                <input type="text" onChange={this.onHandleChange}/>
+                {isLoad && <span>Loading...</span>}
+
+                {!isLoad && 
+                <select onChange={this.onHandlerSelChange}>
+                    <option></option>
+                    {options}
+                </select>
+                }
             </div>
         );
     }
